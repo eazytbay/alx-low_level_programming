b4456bc74473129500e75881ddcc533739d0ce00
eazytbay <folaeazy@gmail.com>
2023-09-07 13:03:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
- * main - C program that displays a line to the standard error
+ * main - C programme that displays a line to the standard error
  * Return: 1 = success
  */
 int main(void)
 {
-if (write(2, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n", 59) != 59)
-
-return (1);
-
+	if (write(2, "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19\n", 59) != 59)
+{
+	return (1);
 }
-
-
-
+return (0);
+}
