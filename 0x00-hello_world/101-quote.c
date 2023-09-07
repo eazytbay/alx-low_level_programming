65d3400b2be699e8931c8440d1fd7b2b034336f6
eazytbay <folaeazy@gmail.com>
2023-09-07 13:45:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\"
--Dora Korpar, 2015-10-19\n";
-write(2, message, 59);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 0);
 return (1);
 }
