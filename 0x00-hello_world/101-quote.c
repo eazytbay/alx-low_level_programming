058523e3d209eb41aff0777d3c2264016be4c3d3
eazytbay <folaeazy@gmail.com>
2023-09-07 11:20:44 -0700

    101-quote.c corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 /**
  * main - C program that displays a line to the standard error
  * Return: 1 = success
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar. 2015-10-19"); 
+if (write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) != 59)
+{
 return (1);
+
+}
+return (0);
+
 }
