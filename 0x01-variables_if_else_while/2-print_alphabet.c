3f9985beafaf6a1d92a13af579ab1455b40507d3
eazytbay <folaeazy@gmail.com>
2023-09-08 09:28:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabeth in lowercase, followed by a new line
+ * Return: 0 = success
+ */
+int main(void)
+{
+char  = 'a'; /*Begin with the firs lowercase alphabeh*/
+/*The loop function is used to print the alphabet*/
+while (alph <= 'z')
+{
+putchar(alph); /* used to print or display the current alphabet*/
+alph++; /* increments and moves to the next letter*/
+}
+putchar('\n'); /* new line character*/
+return (0);
+}
