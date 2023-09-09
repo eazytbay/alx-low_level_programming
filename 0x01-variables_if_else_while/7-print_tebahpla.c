44b21062228a71b7c66c416106687127fab99f5b
eazytbay <folaeazy@gmail.com>
2023-09-08 18:42:21 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabet in reverse order
+ * Return: 0 = success
+ */
+int main(void)
+{
+char alpha_bet = 'z';
+while (alpha_bet >= 'a')
+{
+putchar(alpha_bet);
+alpha_bet--
+}
+putchar('\n');
+return (0);
+}
