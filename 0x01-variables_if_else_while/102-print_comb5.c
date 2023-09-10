708724c4e2cebf2a5889e40b21ff7b359b9e1780
eazytbay <folaeazy@gmail.com>
2023-09-10 13:06:16 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,26 +5,40 @@
  */
 int main(void)
 {
-int num1 = 0;
-while (num1 <= 99)
+int tens1 = 0;
+int ones1;
+int tens2;
+int ones2;
+while (tens1 <= 9)
 {
-int num2 = num1;
-while (num2 <= 99)
+ones1 = 0;
+while (ones1 <= 9)
 {
-putchar((num1 / 10) + '0');   /* Print the tens digit of num1*/
-putchar((num1 % 10) + '0');   /*Print the ones digit of num1*/
-putchar(' ');                 /* Print a space*/
-putchar((num2 / 10) + '0');   /* Print the tens digit of num2*/
-putchar((num2 % 10) + '0');   /* Print the ones digit of num2*/
-if (num1 < 99 || num2 < 99)
+tens2 = tens1;
+while (tens2 <= 9)
 {
-putchar(',');   /* Print a comma*/
-putchar(' ');   /* Print a space*/
+ones2 = ones1 + 1;
+while (ones2 <= 9)
+{
+putchar('0' + tens1);
+putchar('0' + ones1);
+ptutchar(' ');
+putchar('0' + tens2);
+putchar('0' + ones2);
+if (!(tens1 == 9 && ones1 == 8 && tens2 == 9 && ones2 == 9))
+{
+putchar(',');
+putchar(' ');
 }
-num2++;
+ones2++;
 }
-num1++;
+tens2++;
 }
-putchar('\n'); /* Print a newline character*/
+ones1++;
+}
+tens1++;
+}
+putchar('\n');
+
 return (0);
 }
