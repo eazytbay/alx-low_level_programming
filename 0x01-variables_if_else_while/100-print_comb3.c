7908d63887bf989f87b4e5378026994d4f44de8d
eazytbay <folaeazy@gmail.com>
2023-09-10 11:15:37 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-char num1 = '0';
-while (num1 <= '8')
+int num1 = 0;
+while (num1 <= 8)
 {
-char num2 = num1 + 1;
-while (num2 <= '9')
+int num2 = num1 + 1;
+while (num2 <= 9)
 {
 putchar(num1);
 putchar(num2);
-if (num1 != '8' || num2 != '9')
+if (num1 != 8 || num2 != 9)
 {
 putchar(',');
 putchar(' ');
