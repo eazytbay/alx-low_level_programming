38829f11f0ba78a84527e45048b74a3ea50d4a77
eazytbay <folaeazy@gmail.com>
2023-09-13 02:25:51 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,30 +1,12 @@
-#include <unistd.h>
-
-/*
- * _putchar - writes a character to the standard output
- * @c: The character to be written
- * Return: 0 On success
- */
-int _putchar(char c) 
-{
-/*
- * print_alphabet - prints the lowercase alphabet followed by a new line
- *
- * Description: This function prints all lowercase letters from 'a' to 'z'
- * in ascending order, followed by a new line character.
+#include "main.h"
+/**
+ * prints alphabet in lowercase
+ * Return: void
  */
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
 _putchar('\n');
-}
-int main(void)
-{
-print_alphabet();
-return (0);
 }
