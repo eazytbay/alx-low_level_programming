8fdbc2a95b493fb17a34ca0b20546ee758492251
eazytbay <folaeazy@gmail.com>
2023-09-13 09:54:29 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+ #include <stdio.h>
+/**
+ * main - A program that prints the first 98 fibonacci
+ * starting with 1 and 2
+ * Return: 0 = success
+ */
+int main(void)
+{
+int n = 98;
+int a = 1, b = 2;
+int i = 1;
+int temp = a + b;
+/*Print the first two Fibonacci numbers (1 and 2)*/
+printf("%d, %d", a, b);
+while (i <= n - 2) /*Subtract 2 to account for the first two numbers*/
+{
+a = b;
+b = temp;
+printf(", %d", b);
+i++;
+}
+printf("\n");
+return 0;
+}
+
