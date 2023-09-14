aca634ea63286e65d0fa7150dafa3dc3fd75a517
eazytbay <folaeazy@gmail.com>
2023-09-14 12:29:13 -0700

    0-main.c and header files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests function that prints if an integer is postive
+ * or negative
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+a = i;
+positive_or_negative(i);
+return (0);
+}
