b10526183cb3af82ee331511935748fd8fe54225
eazytbay <folaeazy@gmail.com>
2023-09-14 09:18:45 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line
+ * @n: Number of times line is printed
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+int a;
+for (a = 0; a < n; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
