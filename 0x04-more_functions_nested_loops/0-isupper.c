3eb74d84ada4656b826c556ae452d174a023f45f
eazytbay <folaeazy@gmail.com>
2023-09-14 08:25:49 -0700

    header files and 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - A function that checks for uppercase characters
+ * @b: The character to be checked
+ * Return: 1 for upper case character and 0 for anything else
+ */
+int _isupper(int b)
+{
+if (b >= 65 &&b <= 90)
+{
+return (1);
+}
+return (0);
+}
