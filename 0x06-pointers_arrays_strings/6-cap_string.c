1f496973abbf94c9d78e2f90fbfeabfd313f1916
eazytbay <folaeazy@gmail.com>
2023-09-20 10:29:44 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,4 +9,23 @@ char *cap_string(char *c)
 int string_num;
 string_num = 0;
 while (c[string_num] != '\0')
-if (c[0] >= 97
+{
+if (c[0] >= 97 && c[0] <= 122)
+{
+c[0] = c[0] - 32;
+}
+if (c[string_num] == ' ' || c[string_num] == '\t' || c[string_num] == '\n'
+|| c[string_num] == ',' || c[string_num] == ';' || c[string_num] == '.'
+|| c[string_num] == '.' || c[string_num] == '!' || c[string_num] == '?'
+|| c[string_num] == '"' || c[string_num] == '(' || c[string_num] == ')'
+|| c[string_num] == '{' || c[string_num] == '}')
+{
+if (c[string_num + 1] >= 97 && c[string_num + 1] <= 122)
+{
+c[string_num + 1] = c[string_num + 1] - 32;
+}
+}
+string_num++;
+}
+return (c);
+}
