f419efc2868cc7fe595ab861df02cc15bb3447c2
eazytbay <folaeazy@gmail.com>
2023-10-02 06:53:42 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A program that prints its name
+ * @argv: argument array
+ * @argc: argument number
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+else
+{
+printf("Program name not found\n");
+}
+return (0);
+}
