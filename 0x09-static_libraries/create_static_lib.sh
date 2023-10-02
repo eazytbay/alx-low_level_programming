08d72d84f8e0eed4de9914befce29f85a0017472
eazytbay <folaeazy@gmail.com>
2023-10-02 05:49:03 -0700

    create_static_lib.sh corrected

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-* for creating the object files
 gcc -wall -pedantic -werror -wextra -c *.c
-
-* for creating the static library
 ar -rc liball.a *.o
-
-* for indexing
 ranlib liball.a
-
-* for listing object files
 ar -t libmy.a
 nm libmy.a
