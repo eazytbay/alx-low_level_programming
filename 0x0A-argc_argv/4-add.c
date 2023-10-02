effc87ef449ff9f5ab86d72efaa6e6d280297183
eazytbay <folaeazy@gmail.com>
2023-10-02 10:49:07 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /**
  * main - A program that adds postive numbers
  * @argv: argument array
@@ -13,7 +14,7 @@ int main(int argc, char *argv[])
 int sum = 0;
 int a = 1;
 int b = 0;
-int num = _atoi(argv[a]);
+int num = atoi(argv[a]);
 if (argc < 2)
 {
 printf("0\n");
@@ -21,17 +22,17 @@ return (0);
 }
 while (a < argc)
 {
+printf("processing argument: %s\n", argv[a]);
 while (argv[a][b] != '\0')
 {
+printf("checking character: %c\n", argv[a][b]);
 if (!isdigit(argv[a][b]))
 {
 printf("Error\n");
-b++;
-}
 return (1);
 }
+b++;
 }
-{
 if (num > 0)
 {
 sum += num;
