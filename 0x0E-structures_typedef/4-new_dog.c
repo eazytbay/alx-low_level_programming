d89a45589658e83164b2323f84ba6ed39c9f6354
eazytbay <folaeazy@gmail.com>
2023-10-10 05:27:05 -0700

    task dog.h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,21 +16,54 @@ if (n_Dog == NULL)
 {
 return (NULL);
 }
-n_Dog->name = malloc(strlen(name) + 1);
+/**
+ * _strlen - A function that computes the length of a string
+ * @n_Dog: Variable pointer to dog_t
+ * @str: The string
+ * Return: The length of string
+ */
+size_t _strlen(const char *str)
+{
+size_t length = 0;
+while (str[length] != '\0')
+{
+length++;
+}
+return (length);
+}
+n_Dog->name = malloc(_strlen(name) + 1);
 if (n_Dog->name == NULL)
 {
 free(n_Dog);
 return (NULL);
 }
-strcpy(n_Dog->name, name);
-n_Dog->owner = malloc(strlen(owner) + 1);
+/**
+ * _strcpy -  a function that copies a string
+ * @dest: buffer sorage
+ * @src: the string source 
+ * Return: The copied string
+ */
+char *_strcpy(char *dest, const char *src)
+{
+    char *d = dest;
+while (*src != '\0')
+{
+*d = *src;
+d++;
+src++;
+}
+*d = '\0';
+return (dest);
+}
+_strcpy(n_Dog->name, name);
+n_Dog->owner = malloc(_strlen(owner) + 1);
 if (n_Dog->owner == NULL)
 {
 free(n_Dog->name);
 free(n_Dog);
 return (NULL);
 }
-strcpy(n_Dog->owner, owner);
+_strcpy(n_Dog->owner, owner);
 n_Dog->age = age;
 return (n_Dog);
 }
