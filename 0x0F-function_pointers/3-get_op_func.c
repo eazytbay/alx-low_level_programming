16a2bbffae8068b14e2af1b5900b85b6a7181372
eazytbay <folaeazy@gmail.com>
2023-10-11 04:56:32 -0700

    calc and op_function file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func -A function that selects the correct function 
+ * to perfomr the operation asked by the user
+ * @s: The argument operator
+ * Return: The function's pointer
+ */
+int (*get_op_func(char *s))(int, int);
+{
+op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - A function that returns the sum of two numbers
+ * @a: The first number to be added
+ * @b: The second number to be added
+ * Return: The sum of the two numbers
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - A function that returns the difference between two numbers
+ * @a: The first number to be subtracted
+ * @b: The second number to be subtracted
+ * Return: The differnce of the two numbers
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - A function that returns the product of two numbers
+ * @a: The first number to be multiplied
+ * @b: The second number to be added
+ * Return: The product of the two numbers
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - A function that divides two numbers
+ * @a: The numerator
+ * @b: The denominator
+ * Return: The result of the division
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - A function that returns the remainder of the division
+ * @a: The first number to be divided
+ * @b: The second number to be divided
+ * Return: The remainder after dividing a and b
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
