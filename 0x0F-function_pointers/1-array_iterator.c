0ac48fc41718c4e67d595d3d588fb9adf3a37b14
eazytbay <folaeazy@gmail.com>
2023-10-11 05:47:22 -0700

    correction of 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,10 +4,13 @@
  * @array: the array
  * @size: size of the array
  * @action: function pointer
+ * Return: Does not return anything
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t x;
+if (array == NULL || action == NULL)
+return;
 for (x = 0; x < size; x++)
 {
 action(array[x]);
