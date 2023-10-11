77bf2ff93a8353257f8352853d9ec3dd3402bae8
eazytbay <folaeazy@gmail.com>
2023-10-11 11:36:28 -0700

    solution to task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * print_opcodes - A function that prints opcodes
- * @num_bytes: The number of bytes to print
- * Return: Error is byte is negative
- */
-void print_opcodes(int num_bytes)
-{
-int x;
-unsigned char *ptr = (unsigned char *)&print_opcodes;
-for (x = 0; x < num_bytes; x++)
-{
-if (x == num_bytes - 1)
-{
-printf("%02hhx\n", ptr[x]);
-}
-else
-{
-printf("%02hhx ", ptr[x]);
-}
-}
-}
 /**
  * main - A program that is capable of printing its own opcodes
  * @argc: argument number
@@ -31,19 +8,28 @@ printf("%02hhx ", ptr[x]);
  */
 int main(int argc, char *argv[])
 {
+int b, x;
+char *ptr;
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-int num_bytes = atoi(argv[1]);
-if (num_bytes < 0)
+b = atoi(argv[1]);
+if (b < 0)
 {
 printf("Error\n");
 exit(2);
 }
-print_opcodes(num_bytes);
+ptr = (char *)main;
+for (x = 0; x < b; x++)
+{
+if (x == b - 1)
+{
+printf("%02hhx\n", ptr[x]);
+break;
+}
+printf("%02hhx ", ptr[x]);
+}
 return (0);
 }
-
-
