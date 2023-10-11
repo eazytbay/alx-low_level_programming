664cfa74a48817535fd32abee2248e4a2d1ed822
eazytbay <folaeazy@gmail.com>
2023-10-11 05:31:33 -0700

    correction of 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - A function that prints a name
  * @name: The name that's to be printed
@@ -6,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+return;
 f(name);
 }
