7227f6de2aaffecaeafeeac2340a1c0a5d782b71
eazytbay <eazytbay@gmail.com>
2023-10-19 13:02:10 -0700

    solution to 100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * printMessage - prints a message before the main function is executed
+ */
+void printMessage(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
