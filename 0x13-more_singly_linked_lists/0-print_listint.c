7e0d08c7745033acf15cab6ff449e6bded91eaf4
eazytbay <eazytbay@gmail.com>
2023-10-23 16:11:58 -0700

    updated solution for 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,10 @@
 size_t print_listint(const listint_t *h)
 {
 size_t node_count = 0;
-while (h)
+for (; h; h = h->next)
 {
 printf("%d\n", h->n);
 node_count++;
-h = h->next;
 }
 return (node_count);
 }
