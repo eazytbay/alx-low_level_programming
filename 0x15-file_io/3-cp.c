402b7cc361145a8ed9770659eef31884ee2f7fb2
eazytbay <eazytbay@gmail.com>
2023-10-30 17:29:22 -0700

    update solution to 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,21 +3,21 @@
 
 /**
  * check_file_errors - A function that Checks and handle file open errors.
- * @src_fd: Source file descriptor.
+ * @file_from: Source file descriptor.
  * @dest_fd: Destination file descriptor.
  * @argv: Command-line arguments.
  * Return: absolutely nothing
  */
-void check_file_errors(int src_fd, int dest_fd, char *argv[])
+void check_file_errors(int file_from, int file_to, char *argv[])
 {
-if (src_fd == -1)
+if (file_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Unable to read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 exit(98);
 }
-if (dest_fd == -1)
+if (file_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Unable to write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
 exit(99);
 }
 }
@@ -29,7 +29,7 @@ exit(99);
  */
 int main(int argc, char *argv[])
 {
-int src_fd, dest_fd, close_err;
+int file_from, file_to, close_err;
 ssize_t num_chars, num_written;
 char buffer[1024];
 if (argc != 3)
@@ -37,29 +37,29 @@ if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-src_fd = open(argv[1], O_RDONLY);
-dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-check_file_errors(src_fd, dest_fd, argv);
-for (num_chars = read(src_fd, buffer, 1024);
+file_from = open(argv[1], O_RDONLY);
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+check_file_errors(file_from, file_to, argv);
+for (num_chars = read(file_from, buffer, 1024);
 num_chars > 0;
-num_chars = read(src_fd, buffer, 1024))
+num_chars = read(file_from, buffer, 1024))
 {
 if (num_chars == -1)
 check_file_errors(-1, 0, argv);
-num_written = write(dest_fd, buffer, num_chars);
+num_written = write(file_to, buffer, num_chars);
 if (num_written == -1)
 check_file_errors(0, -1, argv);
 }
-close_err = close(src_fd);
+close_err = close(file_from);
 if (close_err == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", src_fd);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 exit(100);
 }
-close_err = close(dest_fd);
+close_err = close(file_to);
 if (close_err == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", dest_fd);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 exit(100);
 }
 return (0);
